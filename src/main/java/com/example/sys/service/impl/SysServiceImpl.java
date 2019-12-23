3dc88f7afdbf1db9c08174b0a7158d99bbf17745
47911 <479115300@qq.com>
2019-12-23 17:20:50 +0800

    20191223

--- src/main/java/com/example/sys/service/impl/SysServiceImpl.java
+++ src/main/java/com/example/sys/service/impl/SysServiceImpl.java
@@ -3,7 +3,7 @@ package com.example.sys.service.impl;
 import com.example.sys.dao.SysUserDao;
 import com.example.sys.entity.SysUser;
 import com.example.sys.service.SysService;
-import com.example.sys.utils.Md5Utils;
+import com.example.sys.utils.MD5;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +16,6 @@ public class SysServiceImpl implements SysService {
     @Override
 
     public SysUser loginUser(String username,String password) {
-        return sysUserDao.loginUser(username, Md5Utils.getMd5Code(password));
+        return sysUserDao.loginUser(username, MD5.MD5Sign(password));
     }
 }
